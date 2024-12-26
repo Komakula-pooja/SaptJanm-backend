43b122dcc748112fcc443dcfbe2cb312d951bcc7
Komakula-pooja <poojithakomakula21@gmail.com>
2024-12-26 13:15:02 +0530

    first commit

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { PrismaClient } from '@prisma/client/edge'
 import { sign } from 'hono/jwt'
-import { signupInput,signinInput } from "@komakula/saptjanam-common";
+import { signupInput,signinInput, updateUserInput } from "@komakula/saptjanam-common";
 
 export const userRouter=new Hono<{
     Bindings:{
@@ -84,3 +84,42 @@ userRouter.post('/signup',async(c)=>{
     }
   })
   
+userRouter.put('/', async(c)=>{
+  const body = await c.req.json();
+  const validationResult = updateUserInput.safeParse(body);
+
+  if(!validationResult.success){
+    c.status(411);
+    return c.json({
+        message: "Inputs not correct",
+        errors: validationResult.error.format(),
+    })
+  }
+
+  const prisma=new PrismaClient({
+    datasourceUrl:c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  try{
+    const user= await prisma.user.update({
+      where:{
+        id:body.id
+      },
+      data:{
+        ...validationResult.data
+      }
+    })
+    const jwt=await sign({
+      id: user.id
+    },c.env.JWT_SECRET);
+
+    return c.json({
+      token:jwt,
+      id:user.id
+    })
+  }catch(e){
+    c.status(411);
+    return c.text('Invalid')
+  }
+  
+})
